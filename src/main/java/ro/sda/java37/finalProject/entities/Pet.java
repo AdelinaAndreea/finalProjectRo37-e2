68ec604e5ef4f33931b5a5754a8f1b831aaf9bf5
DESKTOP-FFRQOG1\G <andronacheelisabeta@yahoo.com>
2022-08-04 21:06:12 +0300

    Pet and Vet created

--- src/main/java/ro/sda/java37/finalProject/entities/Pet.java
+++ src/main/java/ro/sda/java37/finalProject/entities/Pet.java
@@ -1,4 +1,14 @@
 package ro.sda.java37.finalProject.entities;
 
+import java.util.Date;
+
 public class Pet {
+private Long id;
+private String name;
+private String race;
+private Date dateOfBirth;
+private Float kilos;
+private Boolean isVaccinated;
+private Client owner;
+
 }
--- src/main/java/ro/sda/java37/finalProject/entities/Veterinarian.java
+++ src/main/java/ro/sda/java37/finalProject/entities/Veterinarian.java
@@ -1,4 +1,11 @@
 package ro.sda.java37.finalProject.entities;
 
+import java.util.List;
+
 public class Veterinarian {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String speciality;
+    private List<Consult> consults;
 }

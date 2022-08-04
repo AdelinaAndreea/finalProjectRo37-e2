e4bd008b3069cd5bfb19a6cf1b92a63b00f277af
Stefania <tudorica.stefania@gmail.com>
2022-08-04 20:57:38 +0300

    consult

--- src/main/java/ro/sda/java37/finalProject/entities/Consult.java
+++ src/main/java/ro/sda/java37/finalProject/entities/Consult.java
@@ -1,4 +1,15 @@
 package ro.sda.java37.finalProject.entities;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Consult {
+
+    private Long id;
+    private Date date;
+    private String description;
+    private Veterinarian veterinarian;
+    private Pet pet;
+    private BigDecimal price;
+
 }

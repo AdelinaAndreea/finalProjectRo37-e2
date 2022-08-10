54004c5d16535e38f9e2d1dc73b377f25f201348
Adelina Andreea Muntean <adelina.muntean2@yahoo.com>
2022-08-10 20:02:54 +0300

    update Consult

--- src/main/java/ro/sda/java37/finalProject/controller/ConsultRestController.java
+++ src/main/java/ro/sda/java37/finalProject/controller/ConsultRestController.java
@@ -10,31 +10,32 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/consult", produces = "application/json")
 public class ConsultRestController {
-    @Autowired
-    private ConsultService consultService;
+  @Autowired
+  private ConsultService consultService;
 
-    @GetMapping()
-    public List<ConsultDto> getAllConsults() {
-        return consultService.getAllConsults();
-    }
+  @GetMapping()
+  public List<ConsultDto> getAllConsults() {
+    return consultService.getAllConsults();
+  }
 
-    @GetMapping("/pet/{petId}")
-    public List<ConsultDto> getAllConsultsByPetId(@PathVariable Long petId) {
-        return consultService.getAllConsultsByPetId(petId);
-    }
+  @GetMapping("/pet/{petId}")
+  public List<ConsultDto> getAllConsultsByPetId(@PathVariable Long petId) {
+    return consultService.getAllConsultsByPetId(petId);
+  }
 
-    @GetMapping("/veterinarian/{vetId}")
-    public List<ConsultDto> getAllConsultsByVeterinarianId(@PathVariable Long vetId) {
-        return consultService.getAllConsultsByVeterinarianId(vetId);
-    }
+  @GetMapping("/veterinarian/{vetId}")
+  public List<ConsultDto> getAllConsultsByVeterinarianId(@PathVariable Long vetId) {
+    return consultService.getAllConsultsByVeterinarianId(vetId);
+  }
 
 
-    @PostMapping
-    public ConsultDto createConsult(@RequestBody ConsultDto consultDto) {
-        return consultService.createConsult(consultDto);
-    }
+  @PostMapping
+  public ConsultDto createConsult(@RequestBody ConsultDto consultDto) {
+    return consultService.createConsult(consultDto);
+  }
 
-    public ConsultDto updateConsult(ConsultDto consultDto){
-      return consultService.updateConsult(consultDto);
-    }
+  @PutMapping()
+  public ConsultDto updateConsult(@RequestBody ConsultDto consultDto) {
+    return consultService.updateConsult(consultDto);
+  }
 }

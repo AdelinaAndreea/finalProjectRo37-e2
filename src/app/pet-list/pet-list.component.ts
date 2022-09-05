a2166b5722d735edefe11aadf05fc28ae2ffca68
Adelina Andreea Muntean <adelina.muntean2@yahoo.com>
2022-09-05 21:22:36 +0300

    implement add pet

--- src/app/pet-list/pet-list.component.ts
+++ src/app/pet-list/pet-list.component.ts
@@ -3,6 +3,7 @@ import { Form, NgForm } from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import { Pet } from '../model/pet';
 import { PetServiceService } from '../service/pet-service.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -16,15 +17,18 @@ export class PetListComponent implements OnInit {
  public isVisible:boolean=false;
 ownerId:any;
 public isAddButtonVisible:boolean=false;
+public myGroup!:FormGroup;
+public isShowPetFormVisible:boolean=false;
 
 
 
 
-  constructor(private petService : PetServiceService, private router: Router,private route: ActivatedRoute) { }
+  constructor(private petService : PetServiceService, private router: Router,private route: ActivatedRoute,private formBuilder:FormBuilder) { }
 
   ngOnInit(){
   this.ownerId=this.route.snapshot.paramMap.get('ownerId');
   console.log(this.ownerId);
+  this.isShowPetFormVisible=false;
   if(this.ownerId==null) {
     this.isAddButtonVisible=false;
     this.petService.findAll().subscribe( data=> {
@@ -48,15 +52,36 @@ public isAddButtonVisible:boolean=false;
     })
   }
   
-  addPet(pet:Pet){
-    this.petService.addPet(pet).subscribe(data =>{
+  addPet(){
+    let petTemp : Pet = new Pet();
+
+    petTemp.name=this.myGroup.get('formPetName')?.value;
+    petTemp.race=this.myGroup.get('formRace')?.value;
+    petTemp.dateOfBirth=this.myGroup.get('formDateOfBirth')?.value;
+    petTemp.kilos=this.myGroup.get('formKilos')?.value;
+    petTemp.isVaccinated=this.myGroup.get('formVaccinated')?.value;
+    petTemp.ownerId=this.ownerId;
+    
+  
+    this.petService.addPet(petTemp).subscribe(data =>{
       this.ngOnInit();
-      console.log("Pet with id "+pet.id+" has been added");
+  
     })
   }
   showPetForm(){
-    //TODO
+    this.isShowPetFormVisible=true;
+    this.myGroup=this.formBuilder.group(
+      {
+          formPetName: new FormControl(),
+          formRace: new FormControl(),
+          formDateOfBirth: new FormControl(),
+          formKilos: new FormControl(),
+          formVaccinated: new FormControl(),
+
+      }
+  )
   }
+  
 
 
 // onAdd(){

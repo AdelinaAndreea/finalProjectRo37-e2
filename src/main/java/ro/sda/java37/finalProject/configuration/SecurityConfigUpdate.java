2583e653e8f8ccef048c2d51973946a19119f021
Adelina Andreea Muntean <adelina.muntean2@yahoo.com>
2022-09-10 17:51:35 +0300

    fix login auth on BE

--- src/main/java/ro/sda/java37/finalProject/configuration/SecurityConfigUpdate.java
+++ src/main/java/ro/sda/java37/finalProject/configuration/SecurityConfigUpdate.java
@@ -1,11 +1,15 @@
 package ro.sda.java37.finalProject.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -17,6 +21,7 @@ import java.util.Arrays;
 import static ro.sda.java37.finalProject.configuration.RequestInterceptor.APP_TOKEN_KEY;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfigUpdate {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -37,7 +42,7 @@ public class SecurityConfigUpdate {
     return http.build();
   }
 
-  @Bean
+
   public InMemoryUserDetailsManager userDetailsService() {
     UserDetails admin = User.withDefaultPasswordEncoder()
       .username("admin")
@@ -46,6 +51,15 @@ public class SecurityConfigUpdate {
       .build();
     return new InMemoryUserDetailsManager(admin);
   }
+
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+      .userDetailsService(userDetailsService())
+      .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+  }
+
 
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
--- src/main/java/ro/sda/java37/finalProject/controller/AuthRestController.java
+++ src/main/java/ro/sda/java37/finalProject/controller/AuthRestController.java
@@ -1,7 +1,8 @@
 package ro.sda.java37.finalProject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,15 @@ public class AuthRestController {
   public final static String APP_TOKEN = "my token";
 
   @Autowired
-  public InMemoryUserDetailsManager inMemoryUserDetailsManager;
+  public AuthenticationManagerBuilder authenticationManager;
 
   @PostMapping("/signin")
   public TokenDto login(@RequestBody LoginDto loginDto) {
+    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+      loginDto.getUsername(), loginDto.getPassword());
+
+    authenticationManager.getObject().authenticate(authenticationToken);
+
     return TokenDto.builder()
       .token(APP_TOKEN)
       .build();
